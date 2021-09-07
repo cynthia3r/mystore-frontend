4fa5e4512b420105c54d55cc0a69b1e56306deb0
Sayantani Chaudhuri <sayantani.chaudhuri@gmail.com>
2021-09-07 21:15:03 +1000

    feat: add navigation and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CartComponent } from './cart/cart.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CartComponent
+    CartComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
