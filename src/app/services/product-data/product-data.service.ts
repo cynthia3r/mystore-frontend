f8dada7e1b9a813e53b113b7b33551aa2bfe91c3
Sayantani Chaudhuri <sayantani.chaudhuri@gmail.com>
2021-09-07 23:57:29 +1000

    feat: create new cart service

--- src/app/services/product-data/product-data.service.ts
+++ src/app/services/product-data/product-data.service.ts
@@ -1,18 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
-import { Product } from '../models/Product';
+import { Product } from '../../models/Product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductDataService {
-  private jsonFileContent = "assets/data.json"
 
   constructor(private http: HttpClient) { }
 
   getProducts(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.jsonFileContent);
+    console.log('inside product service');
+    return this.http.get<Product[]>("../../assets/data.json");
   }
 }
