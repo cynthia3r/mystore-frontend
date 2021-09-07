c250a0fc6ec7313b256dfbd111e79fea34cbfa4b
Sayantani Chaudhuri <sayantani.chaudhuri@gmail.com>
2021-09-07 21:16:21 +1000

    feat: add data models

---
+++ src/app/models/CustomerData.ts
@@ -0,0 +1,11 @@
+export class CustomerData {
+  fullname: string;
+  address: string;
+  creditCardNumber: string;
+
+  constructor() {
+      this.fullname = "";
+      this.address = "";
+      this.creditCardNumber = "";
+  }
+}
