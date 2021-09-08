603441b32529f29da1172ab8372c365472fe7ae1
Sayantani Chaudhuri <sayantani.chaudhuri@gmail.com>
2021-09-09 01:13:47 +1000

    feat: add functionality to cart service

--- src/app/services/cart/cart.service.ts
+++ src/app/services/cart/cart.service.ts
@@ -1,9 +1,48 @@
 import { Injectable } from '@angular/core';
+import { Product, CartItem } from 'src/app/models/Product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
+  cart: CartItem[] = [];
+
+  getCartItems(): CartItem[] {
+    return this.cart;
+  }
+
+  addProduct(product: Product, quantity: number): void {
+    const itemInCart:CartItem | undefined = this.cart.find(({id}) => id === product.id);
+
+    if(!itemInCart) {
+      this.cart.push({
+        ...product,
+        quantity
+      });
+    } else {
+      const itemInCartIndex: number = this.cart.indexOf(itemInCart);
+      const updatedItem: CartItem = {
+        ...itemInCart,
+        quantity: itemInCart.quantity + quantity
+      };
+      this.cart.splice(itemInCartIndex, 1, updatedItem);
+
+    }
+    alert("Product added to cart!");
+  }
+
+  removeProduct(productId: number): void {
+    this.cart = this.cart.filter(({id}) => id!== productId);
+    alert("Product removed from cart!");
+  }
+
+  getTotalPrice(): number {
+    let totalPrice = 0;
+    this.cart.forEach(({ price, quantity }) => {
+      totalPrice += price* quantity;
+    });
+    return totalPrice;
+  }
 
   constructor() { }
 }
