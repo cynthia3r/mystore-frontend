312baf726de9eb84c9f6fc12c6abbec78f0c01d9
Sayantani Chaudhuri <sayantani.chaudhuri@gmail.com>
2021-09-09 04:09:51 +1000

    feat: add navigation route for order confirmation from cart component

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CartItem } from '../../models/Product';
+import { Customer } from '../../models/Customer';
+import { CartService } from '../../services/cart/cart.service';
 
 @Component({
   selector: 'app-cart',
@@ -6,10 +10,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
+  cart: CartItem[] = [];
+  totalPrice: number = 0;
+  customer: Customer;
+  success: boolean = false;
 
-  constructor() { }
+  constructor(private cartService: CartService, private router: Router) {
+    this.customer = {
+      fullName: '',
+      address: '',
+      creditCardNumber: ''
+    };
+   }
 
   ngOnInit(): void {
+    this.cart = this.cartService.getCartItems();
+    this.totalPrice = this.cartService.getTotalPrice();
+  }
+
+  removeItemFromCart(productId: number) {
+    this.cartService.removeProduct(productId);
+    this.cart = this.cartService.getCartItems();
+    this.totalPrice = this.cartService.getTotalPrice();
+  }
+
+  registerCustomerDetails(customer: Customer) {
+    this.customer = customer;
+    this.success = true;
+    this.router.navigateByUrl(`confirmation/${this.customer.fullName}/${this.totalPrice}`);
   }
 
 }
