89d30f2f422b501d8e87c3961644b8738fd5fa67
Sayantani Chaudhuri <sayantani.chaudhuri@gmail.com>
2021-09-09 04:07:56 +1000

    feat: generate and implement order confirmation component

--- src/app/components/confirmation/confirmation.component.ts
+++ src/app/components/confirmation/confirmation.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-confirmation',
   templateUrl: './confirmation.component.html',
   styleUrls: ['./confirmation.component.css']
 })
 export class ConfirmationComponent implements OnInit {
+  fullName:string | null = '';
+  totalPrice: number| null = 0;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      this.fullName = params.get('fullName');
+      this.totalPrice = Number(params.get('totalPrice'));
+    })
+
   }
 
 }
