e58a3d584a0da7f1f844243d02bf0c6204417fd1
Sayantani Chaudhuri <sayantani.chaudhuri@gmail.com>
2021-09-09 02:18:49 +1000

    feat: implement add to cart functionality for  product item detail component

--- src/app/components/product-item-detail/product-item-detail.component.ts
+++ src/app/components/product-item-detail/product-item-detail.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../../models/Product';
+import { ProductDataService } from 'src/app/services/product-data/product-data.service';
+import { CartService } from 'src/app/services/cart/cart.service';
 
 @Component({
   selector: 'app-product-item-detail',
@@ -6,10 +10,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-item-detail.component.css']
 })
 export class ProductItemDetailComponent implements OnInit {
+  product: Product;
+  productId: number = 0;
+  quantity: number = 1;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private productDataService: ProductDataService,
+    private cartService: CartService) {
+      this.product = {
+        id: 0,
+        name:  '',
+        price: 0,
+        url: '',
+        description: ''
+      };
+    }
 
   ngOnInit(): void {
+
+    this.route.paramMap.subscribe(params => {
+      this.productId = Number(params.get('id'));
+    })
+
+    this.productDataService.getProducts().subscribe((res: Product[]) => {
+      this.product =res.filter(({id}) => id === this.productId)[0];
+    });
+  }
+
+  addToCart():void {
+    this.cartService.addProduct(this.product, this.quantity);
   }
 
 }
